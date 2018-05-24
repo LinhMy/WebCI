1b04f7ec32ec35e6905c563618e61593e1806801
Mỹ Linh <linhnghicong@gmail.com>
2018-05-24 15:04:43 +0700

    add form succeed when upload succeed

---
+++ application/views/admin/depot/succeed.php
@@ -0,0 +1,15 @@
+<?php
+// load ra file head
+$this->load->view('admin/product/head', $this->data);
+?>
+<div class="line"></div>
+
+<div class="wrapper">
+
+    <!-- Báo tải dữ liệu lên thành công -->
+    <h3 style="margin-top: 15px; color: #104E8B">Tải dữ liệu thành công!</h3>
+    <br />
+    <a href="<?php $base_url?>/WebCI/admin/product">
+        Quay lại trang quản lý sản phẩm
+    </a>
+</div>
