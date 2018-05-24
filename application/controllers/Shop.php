9142e54c6ea610085aecb31a8c27d2157a861dc3
Mỹ Linh <linhnghicong@gmail.com>
2018-05-24 09:06:10 +0700

    add contact
    + form contact
    + mapp view
    + info shop

--- application/controllers/Shop.php
+++ application/controllers/Shop.php
@@ -4,7 +4,27 @@
         {
             $this->data['temp'] = 'site/shop/adress';
             $this->load->view('site/layout',$this->data);
-        }
+            if($this->input->post('submit'))
+		    {
+		      //lay thong tin form lien he
+		        $name = $this->input->post('name');
+		        $namemail = $this->input->post('namemail');
+		        $content = $this->input->post('content');
+		        $data_insert = array('name' => $name, 'email' => $namemail, 'content'=> $content, 'reply'=> 'FALSE', 'date'=> date('Y-m-d H:i:s'));
+		          //chen vao DB
+		        if($this->contact_model->insert_contact($data_insert))
+		        { 
+		          //insert thanh cong
+		          $url =$base_url."/";// quay lai trang lien he
+		          redirect($url);
+		        }
+		        else
+		        {
+		          //insert khong thanh cong
+
+		        }
+		    }
+		}
 
     }
 ?>
