9f029451406ba30a661f9de2b1d5c00b1d8bafc2
langkhachsaka <tranhoai09031997@gmail.com>
2018-05-24 14:37:43 +0700

    fix error address

--- application/views/site/left.php
+++ application/views/site/left.php
@@ -1,58 +1,53 @@
-
 <div class="header-control">
 
-<div class="vertical-menu-wapper">
-    <div class="box-vertical-megamenus" data-items="10">
-        <h4 class="title"><span class="text">Danh mục sản phẩm</span> <span class="bar"><i class="fa fa-bars"></i></span></h4>
-        <div class="verticalmenu-content">
-            <ul class="kt-nav verticalmenu-list" >
-            <?php $i = 12; foreach ($category_list as $row): ?>
-                <li class="menu-item-has-children">
-                    <a href="<?php echo base_url('san-pham/danh-muc/'.seoname($row->category_name).'/'.$row->category_id); ?>">
+    <div class="vertical-menu-wapper">
+        <div class="box-vertical-megamenus" data-items="10">
+            <h4 class="title"><span class="text">Danh mục sản phẩm</span> <span class="bar"><i class="fa fa-bars"></i></span></h4>
+            <div class="verticalmenu-content">
+                <ul class="kt-nav verticalmenu-list" >
+                    <?php $i = 12; foreach ($category_list as $row): ?>
+                    <li class="menu-item-has-children">
+                        <a href="<?php echo base_url('san-pham/danh-muc/'.seoname($row->category_name).'/'.$row->category_id); ?>">
                         <span class="menu-icon"><img src="<?php echo base_url('upload'); ?>/icons/<?php echo $i; ?>.png" alt="">
                         </span><?php echo $row->category_name; ?>
-                    </a>
-                    <ul class="sub-menu" ">
-                    <?php foreach ($row->subs as $subs){ ?>
-                        <li><a href="<?php echo base_url('san-pham/danh-muc/'.seoname($subs->category_name).'/'.$subs->category_id); ?>"><?php echo $subs->category_name; ?></a></li>
-                    <?php } ?>
-                    </ul>
+                        </a>
+                        <ul class="sub-menu" ">
+                        <?php foreach ($row->subs as $subs){ ?>
+                    <li><a href="<?php echo base_url('san-pham/danh-muc/'.seoname($subs->category_name).'/'.$subs->category_id); ?>"><?php echo $subs->category_name; ?></a></li>
+                <?php } ?>
+                </ul>
                 </li>
-            <?php $i++; endforeach; ?>
-            </ul>
-            <a data-closetext="Close" data-opentext="View All Categories" class="viewall closed" href="#">View All Categories</a>
+                <?php $i++; endforeach; ?>
+                </ul>
+                <a data-closetext="Close" data-opentext="View All Categories" class="viewall closed" href="#">View All Categories</a>
+            </div>
         </div>
     </div>
-</div>
-<div class="main-menu-wapper">
-    <a class="mobile-navigation" href="#">
+    <div class="main-menu-wapper">
+        <a class="mobile-navigation" href="#">
 						<span class="icon">
 							<span></span>
 							<span></span>
 							<span></span>
 						</span>
-        Main Menu
-    </a>
-    <ul class="kt-nav main-menu clone-main-menu">
-        <li class="menu-item-has-children">
-            <a href="<?php echo base_url(); ?>">Trang Chủ</a>
-
-        </li>
-        <li class="menu-item-has-children item-megamenu">
-            <a href="<?php echo base_url(); ?>chi-tiet-san-pham/tui-gucci-1/tui-gucci-1/6">Sản Phẩm</a>
-
-        </li>
-
-
-        <?php if(isset($user_info) && $user_info !=  ''): ?>
-            <li><a style="text-transform: capitalize;" href="<?php echo site_url('user/index'); ?>">Xin chào:<?php echo $user_info->name; ?></a></li>
-            <li><a style="text-transform: capitalize;" href="<?php echo site_url('user/logout'); ?>">Đăng Xuất</a></li>
-        <?php else: ?>
-        <li><a href="<?php echo site_url('user/login') ?>">Đăng Nhập</a></li>
-        <li><a href="<?php echo site_url('user/register')?>">Đăng Ký</a></li>
-        <?php endif; ?>
-        <li class="menu-item-has-children">
-            <a href="<?php echo base_url('shop/adress'); ?>">Liên hệ</a>
-    </ul>
-</div>
+            Main Menu
+        </a>
+        <ul class="kt-nav main-menu clone-main-menu">
+            <li class="menu-item-has-children">
+                <a href="<?php echo base_url(); ?>">Trang Chủ</a>
+            </li>
+            <li class="menu-item-has-children item-megamenu">
+                <a href="<?php echo base_url(); ?>chi-tiet-san-pham/tui-gucci-1/tui-gucci-1/6">Sản Phẩm</a>
+            </li>
+            <?php if(isset($user_info) && $user_info !=  ''): ?>
+                <li><a style="text-transform: capitalize;" href="<?php echo site_url('user/index'); ?>">Xin chào:<?php echo $user_info->name; ?></a></li>
+                <li><a style="text-transform: capitalize;" href="<?php echo site_url('user/logout'); ?>">Đăng Xuất</a></li>
+            <?php else: ?>
+                <li><a href="<?php echo site_url('user/login') ?>">Đăng Nhập</a></li>
+                <li><a href="<?php echo site_url('user/register')?>">Đăng Ký</a></li>
+            <?php endif; ?>
+            <li class="menu-item-has-children">
+                <a href="<?php echo base_url('shop/adress'); ?>">Liên hệ</a>
+        </ul>
+    </div>
 </div>
--- application/views/site/shop/adress.php
+++ application/views/site/shop/adress.php
@@ -1,14 +1,12 @@
 
-
-
-<div class="tab-catalog" style="width: 1300px; margin: 0px auto; height: auto;">
-    <div class="container">      
+<div class="tab-catalog" style="width: 1340px; margin: 0px auto; height: auto;">
+    <div class="container">
 
         <div class="cell">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3779.602355748115!2d105.68745431463257!3d18.68183168731527!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3139cddfa28a58d5%3A0x997b483b6a439dd!2zMTAgRHV5IFTDom4sIEjGsG5nIFBow7pjLCBUaMOgbmggcGjhu5EgVmluaCwgTmdo4buHIEFuLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1527125511854" width="575" height="240" frameborder="0" style="border:0" allowfullscreen></iframe>
 
         </div>
-         <div class="row" style="margin-top: -241px">
+        <div class="row" style="margin-top: -241px">
             <h2 class="fa fa-spinner"> Thông tin shop <i>NatulieShop</i></h2><br />
             <h3 class="fa fa-facebook-square"> facebook: https://www.facebook.com</h3><br />
             <h3 class="fa fa-check-square"> Chuyên cung cấp túi xách xỉ và lẻ, mỹ phẩm xuất xứ thailand 100%, giá cả và chất lượng luôn cam kết tốt nhất cho khách hàng</h3><br />
@@ -16,13 +14,13 @@
             <img src="<?php echo base_url('upload/logos/2.png') ?>">
         </div>
         <div class="contact" style="margin-left:300px">
-          <form action="" method="POST">
-            <input type="text" id="name" name="name" placeholder="Tên của bạn...">
-            <input type="text" id="email" name="mailname" placeholder="Địa chỉ mail của bạn..">
-            <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px" rows="5"></textarea><br>
-            <input type="submit" name = "submit" value="Gửi phản hồi">
+            <form action="" method="POST">
+                <input type="text" id="name" name="name" placeholder="Tên của bạn...">
+                <input type="text" id="email" name="mailname" placeholder="Địa chỉ mail của bạn..">
+                <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px" rows="5"></textarea><br>
+                <input type="submit" name = "submit" value="Gửi phản hồi">
 
-          </form>
+            </form>
         </div>
         <div class="test">
             <form action="" method="POST">
@@ -59,7 +57,7 @@
 
     }
     div.cell {
-        
+
         border-bottom: 2px solid #003399;
         width: 850px;
         padding-bottom: 4px;
@@ -68,7 +66,7 @@
         float: left;
     }
     div.row {
-        
+
         border-bottom: 2px solid #003399;
         width: 300px;
         padding-bottom: 4px;
@@ -89,7 +87,7 @@
         margin-top: -434px;
     }
     div.contact {
-        
+
         border-bottom: 2px solid #003399;
         width: 800px;
         padding-bottom: 4px;
@@ -97,14 +95,14 @@
         color: #000;
     }
     input[type=text], select, textarea {
-    width: 50%; /* Full width */
-    padding: 12px; /* Some padding */  
-    border: 1px solid #ccc; /* Gray border */
-    border-radius: 4px; /* Rounded borders */
-    box-sizing: border-box; /* Make sure that padding and width stays in place */
-    margin-top: 6px; /* Add a top margin */
-    margin-bottom: 16px; /* Bottom margin */
-    resize: vertical /* Allow the user to vertically resize the textarea (not horizontally) */
+        width: 50%; /* Full width */
+        padding: 12px; /* Some padding */
+        border: 1px solid #ccc; /* Gray border */
+        border-radius: 4px; /* Rounded borders */
+        box-sizing: border-box; /* Make sure that padding and width stays in place */
+        margin-top: 6px; /* Add a top margin */
+        margin-bottom: 16px; /* Bottom margin */
+        resize: vertical /* Allow the user to vertically resize the textarea (not horizontally) */
     }
 
     /* Style the submit button with a specific background color etc */
@@ -128,6 +126,6 @@
         background-color: #f2f2f2;
         padding: 20px;
     }
-        
+
 
 </style>
