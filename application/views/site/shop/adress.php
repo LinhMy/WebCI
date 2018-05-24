c8d2f7750a1416918f23f2476fe9f6beb0f1ecf0
langkhachsaka <tranhoai09031997@gmail.com>
2018-05-24 09:31:19 +0700

    fix div googlemap
    shop/address.php

--- application/views/site/shop/adress.php
+++ application/views/site/shop/adress.php
@@ -4,28 +4,34 @@
 <div class="tab-catalog" style="width: 1300px; margin: 0px auto; height: auto;">
     <div class="container">      
 
-        <div class="cell"  >
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3779.602355748115!2d105.68745431463257!3d18.68183168731527!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3139cddfa28a58d5%3A0x997b483b6a439dd!2zMTAgRHV5IFTDom4sIEjGsG5nIFBow7pjLCBUaMOgbmggcGjhu5EgVmluaCwgTmdo4buHIEFuLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1527125511854" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <div class="cell">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3779.602355748115!2d105.68745431463257!3d18.68183168731527!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3139cddfa28a58d5%3A0x997b483b6a439dd!2zMTAgRHV5IFTDom4sIEjGsG5nIFBow7pjLCBUaMOgbmggcGjhu5EgVmluaCwgTmdo4buHIEFuLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1527125511854" width="575" height="240" frameborder="0" style="border:0" allowfullscreen></iframe>
 
         </div>
-         <div class="row"  >
+         <div class="row" style="margin-top: -241px">
             <h2 class="fa fa-spinner"> Thông tin shop <i>NatulieShop</i></h2><br />
             <h3 class="fa fa-facebook-square"> facebook: https://www.facebook.com</h3><br />
             <h3 class="fa fa-check-square"> Chuyên cung cấp túi xách xỉ và lẻ, mỹ phẩm xuất xứ thailand 100%, giá cả và chất lượng luôn cam kết tốt nhất cho khách hàng</h3><br />
             <i class="fa fa-volume-control-phone"> Điện thoại: 0905770963 - 01674717919</i><br />
             <img src="<?php echo base_url('upload/logos/2.png') ?>">
         </div>
-        <div class="contact" style="margin-left:300">
+        <div class="contact" style="margin-left:300px">
           <form action="" method="POST">
-            <label for="fname">Tên</label>
             <input type="text" id="name" name="name" placeholder="Tên của bạn...">
-            <label for="email">Email</label>
             <input type="text" id="email" name="mailname" placeholder="Địa chỉ mail của bạn..">
-            <label for="subject">Nội dung</label>
-            <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px" rows="5"></textarea>
+            <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px" rows="5"></textarea><br>
             <input type="submit" name = "submit" value="Gửi phản hồi">
 
           </form>
+        </div>
+        <div class="test">
+            <form action="" method="POST">
+                <input type="text" id="name" name="name" placeholder="Tên của bạn...">
+                <input type="text" id="email" name="mailname" placeholder="Địa chỉ mail của bạn..">
+                <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px" rows="5"></textarea><br>
+                <input type="submit" name = "submit" value="Gửi phản hồi">
+
+            </form>
         </div>
 
     </div>
@@ -55,7 +61,7 @@
     div.cell {
         
         border-bottom: 2px solid #003399;
-        width: 800px;
+        width: 850px;
         padding-bottom: 4px;
         font-weight: 800;
         color: #000;
@@ -64,11 +70,23 @@
     div.row {
         
         border-bottom: 2px solid #003399;
-        width: 500px;
+        width: 300px;
         padding-bottom: 4px;
         font-weight: 800;
         color: #000;
         float: right;
+        margin-left: 300px
+    }
+    .test {
+
+        border-bottom: 2px solid #003399;
+        width: 300px;
+        padding-bottom: 4px;
+        font-weight: 800;
+        color: #000;
+        float: right;
+        margin-left: 300px;
+        margin-top: -434px;
     }
     div.contact {
         
