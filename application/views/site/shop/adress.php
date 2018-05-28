3eb69a966fadd6d8139e8ef0370efeb5e27c53be
Mỹ Linh <linhnghicong@gmail.com>
2018-05-28 10:46:20 +0700

    change display contact
    + site/shop/address.php

--- application/views/site/shop/adress.php
+++ application/views/site/shop/adress.php
@@ -1,38 +1,43 @@
 
 <div class="tab-catalog" style="width: 1340px; margin: 0px auto; height: auto;">
-    <div class="container">
+    <div class="container" style="border-bottom: 2px solid #003399;">
 
         <div class="cell">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3779.602355748115!2d105.68745431463257!3d18.68183168731527!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3139cddfa28a58d5%3A0x997b483b6a439dd!2zMTAgRHV5IFTDom4sIEjGsG5nIFBow7pjLCBUaMOgbmggcGjhu5EgVmluaCwgTmdo4buHIEFuLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1527125511854" width="575" height="240" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3779.602355748115!2d105.68745431463257!3d18.68183168731527!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3139cddfa28a58d5%3A0x997b483b6a439dd!2zMTAgRHV5IFTDom4sIEjGsG5nIFBow7pjLCBUaMOgbmggcGjhu5EgVmluaCwgTmdo4buHIEFuLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1527125511854" width="635" height="400" frameborder="0" style="border:1" allowfullscreen></iframe>
 
         </div>
-        <div class="row" style="margin-top: -241px">
+        <div class="row" >
+            <br />
             <h2 class="fa fa-spinner"> Thông tin shop <i>NatulieShop</i></h2><br />
             <h3 class="fa fa-facebook-square"> facebook: https://www.facebook.com</h3><br />
-            <h3 class="fa fa-check-square"> Chuyên cung cấp túi xách xỉ và lẻ, mỹ phẩm xuất xứ thailand 100%, giá cả và chất lượng luôn cam kết tốt nhất cho khách hàng</h3><br />
+            <h3 class="fa fa-check-square"> Chuyên cung cấp túi xách xỉ và lẻ, mỹ phẩm xuất xứ thailand 100%.
+            <br /> Giá cả và chất lượng luôn cam kết tốt nhất cho khách hàng</h3><br />
             <i class="fa fa-volume-control-phone"> Điện thoại: 0905770963 - 01674717919</i><br />
             <img src="<?php echo base_url('upload/logos/2.png') ?>">
         </div>
-        <div class="contact" style="margin-left:300px">
+
+
+        <div class="contact" >
             <form action="" method="POST">
                 <input type="text" id="name" name="name" placeholder="Tên của bạn...">
                 <input type="text" id="email" name="mailname" placeholder="Địa chỉ mail của bạn..">
                 <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px" rows="5"></textarea><br>
-                <input type="submit" name = "submit" value="Gửi phản hồi">
+                <input type="submit" name = "submit" value="Gửi phản hồi của bạn cho Shop">
 
             </form>
         </div>
-        <div class="test">
-            <form action="" method="POST">
-                <input type="text" id="name" name="name" placeholder="Tên của bạn...">
-                <input type="text" id="email" name="mailname" placeholder="Địa chỉ mail của bạn..">
-                <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px" rows="5"></textarea><br>
-                <input type="submit" name = "submit" value="Gửi phản hồi">
-
-            </form>
+        <div class="question">
+            <br />
+            <h4>Câu hỏi thường gặp:</h4>
+            <br />
+            <a href="">Làm thế nào để có thể biết sản phẩm nào đang được khuyến mại hoặc Shop đang có chương trình ưu đãi nào?</a>
+            <br />
+            <a href="">Phí vận chuyển được tính như thế nào?</a>
+            <br />
+            <a href="">Làm thế nào để mua hàng?</a>
         </div>
 
-    </div>
+        </div>
 </div>
 
 
@@ -41,7 +46,7 @@
     div.row h2{
         font-size: 15px;
         border-bottom: 2px solid #003399;
-        width: 200px;
+        /*width: 200px;*/
         padding-bottom: 4px;
         font-weight: 800;
         color: #000;
@@ -58,8 +63,8 @@
     }
     div.cell {
 
-        border-bottom: 2px solid #003399;
-        width: 850px;
+       /* border-bottom: 2px solid #003399;*/
+        width: 640px;
         padding-bottom: 4px;
         font-weight: 800;
         color: #000;
@@ -67,32 +72,32 @@
     }
     div.row {
 
-        border-bottom: 2px solid #003399;
-        width: 300px;
+       /*border-bottom: 2px solid #003399;*/
         padding-bottom: 4px;
         font-weight: 800;
         color: #000;
         float: right;
-        margin-left: 300px
+        
     }
-    .test {
+    .question {
 
-        border-bottom: 2px solid #003399;
-        width: 300px;
-        padding-bottom: 4px;
-        font-weight: 800;
-        color: #000;
-        float: right;
+       /* border-bottom: 2px solid #003399;*/
+            
         margin-left: 300px;
         margin-top: -434px;
+        padding-bottom: 4px;
+        font-weight: 800;
+        color: #000;
+        float: right;
     }
     div.contact {
 
-        border-bottom: 2px solid #003399;
+        /*border-bottom: 2px solid #003399;*/
         width: 800px;
         padding-bottom: 4px;
         font-weight: 800;
         color: #000;
+        float: left;
     }
     input[type=text], select, textarea {
         width: 50%; /* Full width */
@@ -121,11 +126,14 @@
     }
 
     /* Add a background color and some padding around the form */
-    .container {
-        border-radius: 5px;
-        background-color: #f2f2f2;
-        padding: 20px;
-    }
+   
 
 
 </style>
+<script type="text/javascript">
+    $(document).ready(function(){
+         document.getElementById("search_price").style.display = "none";
+         document.getElementById("danhmuc").style.display ="none"   
+    });
+
+</script>
