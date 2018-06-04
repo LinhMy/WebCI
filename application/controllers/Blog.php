c52892a66af02fdab91de77a761efa7c29d4f932
Mỹ Linh <linhnghicong@gmail.com>
2018-06-04 15:26:03 +0700

    +add blog
    +Blog_model
    +Blog_controler:

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -4,11 +4,21 @@
         function __construct()
         {
             parent::__construct();
+            $this->load->model('blog_model');
         }
         function index()
         {
+            //lay danh sach cac bai viet de hien thi theo thoi gian moi nhat
+            $this->data['post_list']=$this->blog_model->get_list_post();
             $this->data['temp'] = 'site/blog/index';
-        $this->load->view('site/layout', $this->data);
+            $this->load->view('site/layout', $this->data);
+        }
+        public function view_post($post_id)
+        {
+            //LAY DU lieu hien thi bai viet
+            $this->data['post']=$this->blog_model->get_post($post_id);            
+            $this->data['temp'] = 'site/blog/view';
+            $this->load->view('site/layout', $this->data);
         }
     }
     ?>
